e4ccd4f6b1850e5f7457c3a70d589c99f9c2d14d
OndrejNepozitek <ondrej.nepozitek@bluestep.se>
2022-12-19 14:17:49 +0100

    chore: bump version to 1.0.5

--- ONe.KitchenDesigner/KitchenDesigner.cs
+++ ONe.KitchenDesigner/KitchenDesigner.cs
@@ -6,7 +6,7 @@ namespace ONe.KitchenDesigner
 {
     internal class KitchenDesigner : IModInitializer
     {
-        public const string Version = "1.0.4";
+        public const string Version = "1.0.5";
 
         private static GameObject GameObject { get; set; }
         
