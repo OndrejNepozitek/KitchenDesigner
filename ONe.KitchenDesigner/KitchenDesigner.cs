43f233bdae8e93b8b7d43ca2771d32ca5cdee80c
OndrejNepozitek <ondrej.nepozitek@bluestep.se>
2022-12-23 09:37:54 +0100

    chore: forgot to bump version to 1.1.0

--- ONe.KitchenDesigner/KitchenDesigner.cs
+++ ONe.KitchenDesigner/KitchenDesigner.cs
@@ -6,7 +6,7 @@ namespace ONe.KitchenDesigner
 {
     internal class KitchenDesigner : IModInitializer
     {
-        public const string Version = "1.0.6";
+        public const string Version = "1.1.0";
 
         private static GameObject GameObject { get; set; }
         
