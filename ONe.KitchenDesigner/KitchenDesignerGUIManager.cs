df21209817fb6c4db5d7ec097fafe632e72987fa
OndrejNepozitek <ondrej.nepozitek@bluestep.se>
2023-01-02 17:43:31 +0100

    feat: scale the GUI so that it is more easily readable on large resolutions

--- ONe.KitchenDesigner/KitchenDesignerGUIManager.cs
+++ ONe.KitchenDesigner/KitchenDesignerGUIManager.cs
@@ -6,6 +6,7 @@ public class KitchenDesignerGUIManager : MonoBehaviour
 {
     private bool _show;
     private static readonly int WindowId = nameof(KitchenDesigner).GetHashCode();
+    public static float Scale { get; private set; } = 1f;
 
     public void Show()
     {
@@ -22,10 +23,13 @@ public class KitchenDesignerGUIManager : MonoBehaviour
     
     private Rect CalculateWindowRect()
     {
+        Scale = Mathf.Max(1f, 0.65f * Screen.height / 500);
+        
         var width = Mathf.Min(Screen.width, 650);
-        var height = Mathf.Min(Screen.height, 400);
+        var height = Mathf.Min(Screen.height, 475);
         var offsetX = Mathf.RoundToInt((Screen.width - width) / 2f);
         var offsetY = Mathf.RoundToInt((Screen.height - height) / 2f);
+        
         return new Rect(offsetX, offsetY, width, height);
     }
 
@@ -34,6 +38,8 @@ public class KitchenDesignerGUIManager : MonoBehaviour
         if (_show)
         {
             var windowRect = CalculateWindowRect();
+
+            GUIUtility.ScaleAroundPivot(new Vector2(Scale, Scale), new Vector2(Screen.width / 2f, Screen.height / 2f));
             
             var backgroundTexture = new Texture2D(1, 1, TextureFormat.RGBAFloat, false); 
             backgroundTexture.SetPixel(0, 0, new Color(0.15f, 0.15f, 0.15f, 1));
